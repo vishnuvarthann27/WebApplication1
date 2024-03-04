f11bf770f49fc40b2b05bb3392ccfd4b6e833628
VISHNUVARTHAN N <vishnuvarthan27011999@gmail.com>
2024-03-05 00:26:15 +0530

    Initial  Commit

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using WebApplication1.Controllers;
+
+namespace TestProject
+{
+    public class UnitTest1
+    {
+        private WeatherForecastController weatherForecastController;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public UnitTest1()
+        {
+            _logger = Substitute.For<ILogger<WeatherForecastController>>();
+            weatherForecastController = new WeatherForecastController(_logger);
+        }
+
+        public ILogger<WeatherForecastController> Get_logger()
+        {
+            return _logger;
+        }
+
+        [Fact]
+        public async void GetNotNull()
+        {
+
+            var value = weatherForecastController.Get();
+            _logger.LogInformation(value.ToString());
+            var result = value as OkObjectResult;
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async void GetOkResult()
+        {
+            var value = weatherForecastController.Get();
+            var result = value as OkObjectResult;
+            Assert.Equal(200, result.StatusCode);
+        }
+    }
+}
